79ab292a5d05cf46d4ccdb77245179064de5387b
ubu11 <uvaisubu11@gmail.com>
2025-11-04 20:49:21 +0530

    [feat]:clerk and hono made a connection

--- apps/Server/src/index.ts
+++ apps/Server/src/index.ts
@@ -1,15 +1,33 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
-import { clerkMiddleware } from "@hono/clerk-auth";
 const app = new Hono();
-import clerkAuth from "./controller/clerkAuth.js";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { api } from "../api.js";
+// import { api } from "../api.js";
 
-// app.use("*", clerkMiddleware({}));
+app.use("*", clerkMiddleware({
+  secretKey: api.CLERK_SECRET_KEY,
+  publishableKey:api.CLERK_PUBLISHABLE_KEY,
+}));
 
-app.get("/", (c) => {
-  // clerkAuth(c);
-  return c.text("Hello Hono!");
+app.get("/", async (c) => {
+  const auth = c.get("clerk");
+
+  try {
+    const user = await auth.users.getUser("user_id_....");
+    console.log(user);
+    return c.json({
+      user,
+    });
+  } catch (error: any) {
+    return c.json(
+      {
+        message: error.message,
+      },
+      404
+    );
+  }
+
 });
 
 serve(
