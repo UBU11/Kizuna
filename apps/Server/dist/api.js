08cabd577685d1bb5ecfdcb02a32011fef986038
ubu11 <uvaisubu11@gmail.com>
2025-11-07 16:42:31 +0530

    [refactor]: replace clerk auth and hono framework -> fastify & better auth(better developer friendly eco system), rewiring with hono is bit overwhelming(plugins official documentation support is limited)

--- apps/Server/dist/api.js
+++
@@ -1,4 +0,0 @@
-export const api = {
-    CLERK_SECRET_KEY: "sk_test_hbK3eXkhJ7Xpm4h6SGiipuO8moubUkCE4HweXWfzQY",
-    CLERK_PUBLISHABLE_KEY: "pk_test_c3VwcmVtZS1waWdsZXQtNDEuY2xlcmsuYWNjb3VudHMuZGV2JA",
-};
