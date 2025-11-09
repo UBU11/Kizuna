8dba23b4b08ef32c1d4c64a19eafca477b73cddc
ubu11 <uvaisubu11@gmail.com>
2025-11-09 12:16:09 +0530

    feat: signup page

---
+++ apps/Client/src/components/sign-up.ts
@@ -0,0 +1,34 @@
+import { authClient } from "../lib/auth-client.ts";
+import { useNavigate } from "react-router-dom";
+
+type User = {
+  email: string;
+  name: string;
+  password: string;
+}
+
+const navigate = useNavigate();
+
+export const SignUp = async({email, password, name}: User) => {
+  const { date, error } = await authClient.signUp.email(
+    {
+      email: email,
+      password: password,
+      name: name,
+      callbackURL: "http://localhost:5173",
+    },
+    {
+      onRequest: (ctx) => {
+        console.log(`loading:${ctx.url}`);
+      },
+      onSuccess: (ctx) => {
+        console.log("request is successful");
+        navigate("/login");
+      },
+
+      onError: (ctx) => {
+        alert(ctx.error.message);
+      },
+    }
+  );
+};
