5634148514187794cacc21da82872a92a6bc9748
ubu11 <uvaisubu11@gmail.com>
2025-11-10 23:41:15 +0530

    feat: websocket init

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -2,10 +2,14 @@ import fastify from "fastify";
 import dotenv from "dotenv";
 import { auth } from "./lib/auth.ts";
 import authProxyHandler from "./handler/authProxyHandler.ts";
+import WSEngine from "./handler/wsEngine.ts";
 import fastifyCors from "@fastify/cors";
+import fastifyWebsocket from "@fastify/websocket";
 dotenv.config();
 
 const server = fastify({ logger: true });
+
+server.register(fastifyWebsocket);
 
 server.register(fastifyCors, {
   origin: process.env.CLIENT_ORIGIN || "http://localhost:5173",
@@ -15,7 +19,7 @@ server.register(fastifyCors, {
   maxAge: 86400,
 });
 
-server.get("/", async (request, reply) => {
+server.get("/home", async (request, reply) => {
   return "Welcome :)";
 });
 
@@ -30,6 +34,8 @@ server.route({
 server.get("/ping", async (request, reply) => {
   return "pong\n";
 });
+
+ server.get("/ws", { websocket: true }, WSEngine);
 
 server.listen({ port: 8080 }, (err, address) => {
   if (err) {
