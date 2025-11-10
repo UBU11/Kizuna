0e8c9411f5d269e306c69a86dbd839bab126f91f
ubu11 <uvaisubu11@gmail.com>
2025-11-10 23:00:15 +0530

    frat: websocket initilized

---
+++ apps/server/src/config/wsEngine.ts
@@ -0,0 +1,18 @@
+import fastify from "fastify";
+import fastifyIO from "fastify-socket.io";
+
+const server = fastify();
+server.register(fastifyIO);
+
+server.get("/", (req, reply) => {
+  server.io.emit("hello");
+});
+
+server.ready().then(() => {
+  // we need to wait for the server to be ready, else `server.io` is undefined
+  server.io.on("connection", (socket) => {
+    // ...
+  });
+});
+
+server.listen({ port: 3000 });
