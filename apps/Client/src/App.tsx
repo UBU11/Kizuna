5a6deea966a201442d810f0261ad6acf604f37f3
UBU11 <uvaisubu11@gmail.com>
2026-01-11 23:56:12 +0530

    chore: remove nx config file

--- apps/Client/src/App.tsx
+++ apps/Client/src/App.tsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from "react-router-dom";
-import AuthPage from "./components/pages/AuthPage";
+import AuthPage from "./components/pages/SignUp";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/auth/:pathname" element={<AuthPage />} />
+        <Route path="/auth/sign-up" element={<AuthPage />} />
       </Routes>
     </>
   );
